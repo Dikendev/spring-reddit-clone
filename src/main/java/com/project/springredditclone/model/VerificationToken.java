8499975876658a5ae2356457359a52153baa1985
Dikendev <diken.dev@gmail.com>
2023-03-28 15:46:21 -0300

    VerificationToken entity / Repository

--- src/main/java/com/project/springredditclone/model/VerificationToken.java
+++ src/main/java/com/project/springredditclone/model/VerificationToken.java
@@ -1,14 +1,18 @@
 package com.project.springredditclone.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Entity
+@Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "token")
-class VerificationToken {
+public class VerificationToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
---
+++ src/main/java/com/project/springredditclone/repository/VerificationTokenRepository.java
@@ -0,0 +1,9 @@
+package com.project.springredditclone.repository;
+
+import com.project.springredditclone.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+}
